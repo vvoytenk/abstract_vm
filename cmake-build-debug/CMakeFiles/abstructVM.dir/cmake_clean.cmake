21257c7b2035cccf008d75ae853e18b309348bac
Victoria Voytenko <vvoytenk@student.unit.ua>
2018-11-20 15:21:09 +0200

    create

---
+++ cmake-build-debug/CMakeFiles/abstructVM.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/abstructVM.dir/main.cpp.o"
+  "abstructVM.pdb"
+  "abstructVM"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/abstructVM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
